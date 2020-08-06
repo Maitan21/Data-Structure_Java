cbe1db5fdc481a57c5f74d4019a450bf26070073
윤상 조 <oklfg2002@gmail.com>
2020-08-06 18:04:14 +0900

    DS01_BASIC1 what a basic calculate

---
+++ DS01/src/Basic_01/Basic_01.java
@@ -0,0 +1,25 @@
+package Basic_01;
+
+import java.util.Scanner;
+
+
+public class Basic_01 {
+	
+	@SuppressWarnings("resource")
+	public static void main(String[]args) {
+		
+		System.out.print("input nums : ");
+		//Scanner °´Ã¼ »ý¼º  
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		//print result 
+		System.out.print("Sum : " + (num1+num2)+"\r");
+		System.out.print("Sub : " + (num1-num2)+"\r");
+		System.out.print("Mul : " + (num1*num2)+"\r");
+		System.out.printf("div : " + "%.2f\n"+"\r",((double)num1/(double)num2));
+		
+	}
+
+}
